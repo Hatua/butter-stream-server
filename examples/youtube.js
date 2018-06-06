9972ab5ab5aca631ab2f6d77e6f8673f0e1cf528
Niv Sardi <xaiki@evilgiggle.com>
2018-06-06 08:46:11 -0300

    lint
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- examples/youtube.js
+++ examples/youtube.js
@@ -1,12 +1,11 @@
 var Streamer = require('../')
-var Stream = new Streamer('https://www.youtube.com/watch?v=1lDDAloe1VQ',
-  {
-    progressInterval: 200,
-    buffer: 100,
-    port: 9999,
-    writeDir: '',
-    index: 'ssss.mp4'
-  })
+var Stream = new Streamer('https://www.youtube.com/watch?v=9FaOKNpAiIM', {
+  progressInterval: 200,
+  buffer: 100,
+  port: 9999,
+  writeDir: '',
+  index: 'ssss.mp4'
+})
 
 Stream.on('progress', onProgress)
 Stream.on('ready', onReady)
